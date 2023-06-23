be80928a7c6538c8f211151ac41bb3cc2d82523d
Sourav <sourav.sarkar@hih7.com>
2023-06-23 10:30:18 +0530

    config env

--- server/util/dataBase.js
+++ server/util/dataBase.js
@@ -1,8 +1,8 @@
-
+require('dotenv').config()
 const Sequlize = require('sequelize');
-const sequelize = new Sequlize('expenses-tracker', 'root','sourav1234#',{
-  host: 'localhost',
-  dialect: 'mysql'
+const sequelize = new Sequlize(process.env.DB_NAME, process.env.DB_USERNAME,process.env.DB_PASSWORD,{
+  host: process.env.DB_HOST ,
+  dialect: process.env.DB_DIALECT
 })
 
 module.exports = sequelize
